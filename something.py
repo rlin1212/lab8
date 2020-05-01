dc81982d65beae8bbaeaa6f703033f847230768e
rlin1212 <59703268+rlin1212@users.noreply.github.com>
2020-05-01 11:44:07 -0700

    Update something.py

--- something.py
+++ something.py
@@ -1,1 +1,9 @@
-def
+playername = input("Please enter a playername:")
+print('Welcome '+playername)
+playerclass = input('Please select a class- KNIGHT, WIZARD, KING: ')
+if playerclass == 'KING':
+    print("You have a crown.")
+elif playerclass == 'WIZARD':
+    print("You have strong magic power.")
+elif playerclass == 'KNIGHT':
+    print("You have strong agility and srength stats.")
